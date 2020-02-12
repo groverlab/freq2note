f8f54686d62ff72962af728099c8cd6f22e499bf
William H.Grover <wgrover@engr.ucr.edu>
2020-02-11 16:11:18 -0800

    added histogram note

---
+++ hist.py
@@ -0,0 +1,14 @@
+"""
+demo.py - demonstration program for freq2note.py
+by William H. Grover | wgrover@engr.ucr.edu | http://groverlab.org
+"""
+
+import freq2note as f2n
+
+freqs = [356.25,]
+notes = ""
+
+for f in freqs:
+    notes = notes + f2n.lilypond(f2n.find_closest_note(f))
+
+f2n.write(notes)
